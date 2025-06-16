0dc296e92f9ff0e1cd1b87a26e7de06fc2e0f8f6
DeCommer <jdecommer@gmail.com>
2025-06-16 11:55:05 -0400

    trial and error

--- js/numberclicker.js
+++ js/numberclicker.js
@@ -2,21 +2,50 @@ const clickAmnttxt = document.getElementById('click-amount');
 const plusBtn = document.getElementById('plus-btn'); 
 const minusBtn = document.getElementById('minus-btn'); 
 const resetBtn = document.getElementById('reset-btn');
+const intervalIn = document.getElementById('interval-in');
+const intervalTxt = document.getElementById('interval-amount');
+
+let clickAmount = 0;
+let intervalAmount = intervalIn.value;
+let intervalincr = 0;
+
+clickAmnttxt.innerHTML = clickAmount;
+intervalTxt.innerHTML = intervalAmount;
 
 const incr = () => {
-    clickAmnttxt.innerHTML ++;
+    clickAmount++
+    clickAmnttxt.innerHTML = clickAmount;
 }
 
 const decr = () => {
-    clickAmnttxt.innerHTML --;
+    clickAmount--
+    clickAmnttxt.innerHTML = clickAmount;
 }
 
 const reset = () => {
+    clickAmount = 0;
+    intervalAmount = 0;
+    intervalincr = 0;
+    intervalIn.value = '';
     clickAmnttxt.innerHTML = 0;
+    intervalTxt.innerHTML = 0;
+}
+
+const setInterval = () => {
+    let intervalAmount = intervalIn.value;
+    if(intervalAmount == clickAmount) {
+        intervalincr++;
+        intervalAmount = clickAmount
+        intervalTxt.innerHTML = intervalincr;
+        console.log(`Interval: ${intervalAmount}`);
+        console.log(`click: ${clickAmount}`);
+        console.log(`Increment: ${intervalincr}`);
+    }
 }
 
 plusBtn.addEventListener('click', () => {
     incr();
+    setInterval();
 });
 
 minusBtn.addEventListener('click', () => {
@@ -26,3 +55,4 @@ minusBtn.addEventListener('click', () => {
 resetBtn.addEventListener('click', () => {
     reset();
 });
+
